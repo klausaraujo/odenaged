8e3086f0106f78994d0464b2baa81af38dd5222e
klausaraujo <klausaraujo@gmail.com>
2022-07-04 17:59:38 -0500

    Last

--- application/views/main.php
+++ application/views/main.php
@@ -35,7 +35,7 @@
                         <div class="iq-card-header d-flex justify-content-between">
                            <div class="iq-header-title card-body ">
                         <h3 style="font-size:22px;" class="text-center">
-                    <b> SISTEMA INTEGRADO DE GESTIÓN ADMINISTRATIVA INSTITUCIONAL   </b>
+                           <b>SISTEMA INTEGRADO DE REGISTRO DE EVENTOS DEL SECTOR EDUCACIÓN - SIRESE</b>
                          </h3>
                            </div>
                         </div>
